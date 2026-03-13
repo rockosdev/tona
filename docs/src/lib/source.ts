14f5614f2f4212e6c004f0aa710cec72a11231b7
guangzan <guangzan1999@outlook.com>
2026-03-13 15:18:34 +0800

    chore: 移除docs目录及其相关文件

--- docs/src/lib/source.ts
+++
@@ -1,9 +0,0 @@
-import { docs } from 'fumadocs-mdx:collections/server'
-import { loader } from 'fumadocs-core/source'
-import { lucideIconsPlugin } from 'fumadocs-core/source/lucide-icons'
-
-export const source = loader({
-  source: docs.toFumadocsSource(),
-  baseUrl: '/docs',
-  plugins: [lucideIconsPlugin()],
-})
